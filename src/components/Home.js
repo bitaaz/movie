6a7e38b60288f08a60ef7a21b0dbc77f3ab286ae
bitaaz <bb_az@yahoo.com>
2021-10-09 12:37:36 +0330

    Start using single line grid list instead of grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,7 @@ import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
 import Button from "./Button";
 import Navbar from "./Navbar";
+import SingleLineGridList from "./HorizontalScrollMenu";
 
 const Home = () => {
   const {
@@ -34,29 +35,33 @@ const Home = () => {
         <HeroImage heros={state.results.slice(0, 6)} />
       ) : null}
       <SearchBar setSearchedItem={setSearchedItem} />
-      <Grid header="Popular Movies">
-        {state.results.map((movie) => (
-          <Thumb
-            key={movie.id}
-            clickable
-            image={
-              movie.poster_path
-                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                : NoImage
-            }
-            movieId={movie.id}
-          />
-        ))}
-      </Grid>
-      {isLoading && <Spinner />}
-      {!isLoading && state.page < state.total_pages && (
-        <Button
-          text="Load More"
-          callback={() => {
-            setLoadMore(true);
-          }}
-        />
-      )}
+      <SingleLineGridList
+        gridItemsInfo={state.results}
+        title="Popular Movies"
+      />
+      {/*<Grid header="Popular Movies">*/}
+      {/*  {state.results.map((movie) => (*/}
+      {/*    <Thumb*/}
+      {/*      key={movie.id}*/}
+      {/*      clickable*/}
+      {/*      image={*/}
+      {/*        movie.poster_path*/}
+      {/*          ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path*/}
+      {/*          : NoImage*/}
+      {/*      }*/}
+      {/*      movieId={movie.id}*/}
+      {/*    />*/}
+      {/*  ))}*/}
+      {/*</Grid>*/}
+      {/*{isLoading && <Spinner />}*/}
+      {/*{!isLoading && state.page < state.total_pages && (*/}
+      {/*  <Button*/}
+      {/*    text="Load More"*/}
+      {/*    callback={() => {*/}
+      {/*      setLoadMore(true);*/}
+      {/*    }}*/}
+      {/*  />*/}
+      {/*)}*/}
     </>
   );
 };
