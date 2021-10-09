e907b94b72dce7b5527b58a6efbb380a49eb3169
bitaaz <bb_az@yahoo.com>
2021-10-09 12:39:03 +0330

    Add private route for authentication check

---
+++ src/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth } from "./contexts/AuthContext";
+import { Route, Navigate } from "react-router-dom";
+
+export function PrivateRoute({ element, path }) {
+  const { currentUser } = useAuth();
+
+  const ele = currentUser !== null ? element : <Navigate to="/login" replace />;
+
+  return <Route path={path} element={ele} />;
+}
+
+export function AuthPrivateRoute({ element, path }) {
+  const { currentUser } = useAuth();
+
+  console.log(currentUser);
+
+  const ele = currentUser !== null ? <Navigate to="/" replace /> : element;
+
+  return <Route path={path} element={ele} />;
+}
