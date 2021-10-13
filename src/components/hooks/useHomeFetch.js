0e5b77cfb4a35733c1c00262337b7a8ceb1cf4bf
bitaaz <bb_az@yahoo.com>
2021-10-13 15:14:38 +0330

    Add distinct functions for fetching each movie categories

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -32,6 +32,16 @@ export const useHomeFetch = () => {
         results:
           page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
       }));
+    } catch (error) {
+      setError(true);
+    }
+    setIsLoading(false);
+  };
+
+  const fetchTopRatedMovies = async (page) => {
+    try {
+      setError(false);
+      setIsLoading(true);
 
       const topRatedMoviesApi = await API.fetchTopRatedMovies(page);
 
@@ -42,6 +52,16 @@ export const useHomeFetch = () => {
             ? [...prev.results, ...topRatedMoviesApi.results]
             : [...topRatedMoviesApi.results],
       }));
+    } catch (error) {
+      setError(true);
+    }
+    setIsLoading(false);
+  };
+
+  const fetchUpcomingMovies = async (page) => {
+    try {
+      setError(false);
+      setIsLoading(true);
 
       const upcomingMoviesApi = await API.fetchUpcomingMovies(page);
 
@@ -52,6 +72,17 @@ export const useHomeFetch = () => {
             ? [...prev.results, ...upcomingMoviesApi.results]
             : [...upcomingMoviesApi.results],
       }));
+    } catch (error) {
+      setError(true);
+    }
+
+    setIsLoading(false);
+  };
+
+  const fetchNowPlayingMovies = async (page) => {
+    try {
+      setError(false);
+      setIsLoading(true);
 
       const nowPlayingMoviesApi = await API.fetchNowPlayingMovies(page);
 
@@ -65,6 +96,7 @@ export const useHomeFetch = () => {
     } catch (error) {
       setError(true);
     }
+
     setIsLoading(false);
   };
 
@@ -84,6 +116,54 @@ export const useHomeFetch = () => {
     fetchMovies(1, searchedItem);
   }, [searchedItem]);
 
+  //top rated movies
+  useEffect(() => {
+    if (!searchedItem) {
+      const sessionState = isPersistedState("topRated");
+
+      if (sessionState) {
+        console.log("top rated from session");
+        setTopRatedMovies(sessionState);
+        return;
+      }
+    }
+    console.log("top rated from api");
+    setTopRatedMovies(initialState);
+    fetchTopRatedMovies(1);
+  }, [searchedItem]);
+
+  //now playing movie
+  useEffect(() => {
+    if (!searchedItem) {
+      const sessionState = isPersistedState("nowPlaying");
+
+      if (sessionState) {
+        console.log("now playing from session");
+        setNowPlayingMovies(sessionState);
+        return;
+      }
+    }
+    console.log("now playing from api");
+    setNowPlayingMovies(initialState);
+    fetchNowPlayingMovies(1);
+  }, [searchedItem]);
+
+  //upcoming movies
+  useEffect(() => {
+    if (!searchedItem) {
+      const sessionState = isPersistedState("upcoming");
+
+      if (sessionState) {
+        console.log("upcoming from session");
+        setUpcomingMovies(sessionState);
+        return;
+      }
+    }
+    console.log("upcoming from api");
+    setUpcomingMovies(initialState);
+    fetchUpcomingMovies(1);
+  }, [searchedItem]);
+
   //load more
   useEffect(() => {
     if (!loadMore) return;
@@ -96,8 +176,11 @@ export const useHomeFetch = () => {
   useEffect(() => {
     if (!searchedItem) {
       sessionStorage.setItem("homeState", JSON.stringify(state));
+      sessionStorage.setItem("topRated", JSON.stringify(topRatedMovies));
+      sessionStorage.setItem("upcoming", JSON.stringify(upcomingMovies));
+      sessionStorage.setItem("nowPlaying", JSON.stringify(nowPlayingMovies));
     }
-  }, [searchedItem, state]);
+  }, [searchedItem, state, topRatedMovies, upcomingMovies, nowPlayingMovies]);
 
   return {
     topRatedMovies,
