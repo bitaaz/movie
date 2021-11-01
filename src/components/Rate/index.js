29ba155a34d6f7b43460a1c609547303e8bf9417
bitaaz <bb_az@yahoo.com>
2021-11-01 16:17:24 +0330

    Add rating design and computaion

--- src/components/Rate/index.js
+++ src/components/Rate/index.js
@@ -1,35 +1,117 @@
 import React, { useState } from "react";
-import { Box, Button, Slider, Typography } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import { gThumbDownAlt, ThumbUpAlt } from "@material-ui/icons";
 
-const Rate = ({ callback }) => {
-  const [value, setValue] = useState(5);
-
-  const handleSliderChange = (event, newValue) => {
-    setValue(newValue);
+const Rate = () => {
+  const [likeState, setLikeState] = useState({
+    alreadyLiked: false,
+    alreadyDisliked: false,
+    likes: 0,
+    dislikes: 0,
+    total_votes: 0,
+  });
+  const handleLike = () => {
+    if (!likeState.alreadyDisliked) {
+      if (!likeState.alreadyLiked) {
+        setLikeState({
+          alreadyLiked: true,
+          alreadyDisliked: likeState.alreadyDisliked,
+          likes: likeState.likes + 1,
+          dislikes: likeState.dislikes,
+          total_votes: likeState.total_votes + 1,
+        });
+      } else {
+        setLikeState({
+          alreadyLiked: false,
+          alreadyDisliked: likeState.alreadyDisliked,
+          likes: likeState.likes - 1,
+          dislikes: likeState.dislikes,
+          total_votes: likeState.total_votes - 1,
+        });
+      }
+    } else {
+      setLikeState({
+        alreadyLiked: true,
+        alreadyDisliked: false,
+        likes: likeState.likes + 1,
+        dislikes: likeState.dislikes - 1,
+        total_votes: likeState.total_votes,
+      });
+    }
   };
+  const handleDislike = () => {
+    if (!likeState.alreadyLiked) {
+      if (!likeState.alreadyDisliked) {
+        setLikeState({
+          alreadyDisliked: true,
+          alreadyLiked: likeState.alreadyDisliked,
+          dislikes: likeState.likes + 1,
+          likes: likeState.likes,
+          total_votes: likeState.total_votes + 1,
+        });
+      } else {
+        setLikeState({
+          alreadyDisliked: false,
+          alreadyLiked: likeState.alreadyLiked,
+          dislikes: likeState.dislikes - 1,
+          likes: likeState.likes,
+          total_votes: likeState.total_votes - 1,
+        });
+      }
+    } else {
+      setLikeState({
+        alreadyLiked: false,
+        alreadyDisliked: true,
+        likes: likeState.likes - 1,
+        dislikes: likeState.dislikes + 1,
+        total_votes: likeState.total_votes,
+      });
+    }
+  };
+
   return (
     <div>
-      <div style={{ display: "flex" }}>
-        <Box width={100}>
-          <Slider
-            min={1}
-            max={10}
-            step={1}
-            value={value}
-            // valueLabelDisplay="auto"
-            onChange={handleSliderChange}
-          />
-        </Box>
-        <Typography style={{ marginLeft: "10px", marginTop: "3px" }}>
-          {value}
-        </Typography>
+      <div style={{ marginTop: "50px", flexGrow: "0.5" }}>
+        <h3>LIKES</h3>
+        <p>
+          {likeState.total_votes > 0 && likeState.dislikes < likeState.likes
+            ? ((likeState.likes - likeState.dislikes) * 100) /
+              likeState.total_votes
+            : 0}
+          % ({likeState.total_votes} votes){" "}
+        </p>
       </div>
-
-      <p>
-        <Button variant="contained" onClick={() => callback(value)}>
-          Rate
-        </Button>
-      </p>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "50px",
+          alignItems: "center",
+        }}
+      >
+        <IconButton
+          style={{
+            color: likeState.alreadyLiked ? "green" : "black",
+            backgroundColor: "white",
+            width: "35px",
+            height: "35px",
+            marginRight: "20px",
+          }}
+          onClick={handleLike}
+        >
+          <ThumbUpAlt style={{ width: "25px", height: "25px" }} />
+        </IconButton>
+        <IconButton
+          style={{
+            color: likeState.alreadyDisliked ? "red" : "black",
+            backgroundColor: "white",
+            width: "35px",
+            height: "35px",
+          }}
+          onClick={handleDislike}
+        >
+          <ThumbDownAlt style={{ width: "25px", height: "25px" }} />
+        </IconButton>
+      </div>
     </div>
   );
 };
