10a83e4294b0d4d968d36316b3daad6e3688b05a
bitaaz <bb_az@yahoo.com>
2021-11-10 16:02:22 +0330

    Add spinner when loading favorite movie

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import { Footer } from "../Footer/Footer";
 import { useAuth } from "../../contexts/AuthContext";
@@ -6,19 +6,28 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import Grid from "../Grid";
 import Thumb from "../Thumb";
-import { Typography } from "@material-ui/core";
+import { Fade, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import Spinner from "../Spinner";
+import { isPersistedState } from "../../helpers";
 
 export const Favorites = () => {
   const { currentUser } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
   const [votedMovies, setVotedMovies] = useState([]);
   let favoriteMovies = [];
+
+  const [changeBackgroundOnHover, setChangeBackgroundOnHover] = useState({
+    isHovered: {},
+  });
 
   const userFavoriteMovies = async () => {
     const events = await firebase.firestore().collection(currentUser.email);
     events.get().then((querySnapshot) => {
       const tempDoc = querySnapshot.docs.map((doc) => {
-        return { ...doc.data() };
+        return { id: doc.id, ...doc.data() };
       });
+      setIsLoading(false);
       setVotedMovies(tempDoc);
     });
   };
@@ -31,22 +40,65 @@ export const Favorites = () => {
     }
   }
 
+  const handleMouseEnter = (index) => {
+    setChangeBackgroundOnHover((prevState) => {
+      return { isHovered: { ...prevState.isHovered, [index]: true } };
+    });
+  };
+
+  const handleMouseLeave = (index) => {
+    setChangeBackgroundOnHover((prevState) => {
+      return { isHovered: { ...prevState.isHovered, [index]: false } };
+    });
+  };
+
+  const { isHovered } = changeBackgroundOnHover;
+
   return (
     <>
       <Navbar />
       <div style={{ marginTop: "100px" }}>
-        <Grid header="Favorites">
-          {favoriteMovies.length > 0 ? (
-            favoriteMovies.map((item) => (
-              <div style={{ display: "block" }}>
-                <Thumb image={item.image} height="350px" />
-                <Typography>{item.title}</Typography>
-              </div>
-            ))
-          ) : (
-            <div>Nothing to show</div>
-          )}
-        </Grid>
+        {isLoading && <Spinner />}
+        {!isLoading && (
+          <Grid header="Favorites">
+            {favoriteMovies.length > 0 ? (
+              favoriteMovies.map((item) => (
+                <div key={item.id} style={{ display: "block" }}>
+                  <div
+                    style={{
+                      position: "relative",
+                    }}
+                    onMouseEnter={() => handleMouseEnter(item.id)}
+                    onMouseLeave={() => handleMouseLeave(item.id)}
+                  >
+                    <Thumb image={item.image} height="350px" />
+                    <Link to={`/${item.id}`}>
+                      <Fade in={isHovered[item.id]} timeout={400}>
+                        <div
+                          style={{
+                            position: "absolute",
+                            transform: "translateX(-50%)",
+                            top: "0",
+                            left: "120px",
+                            width: "270px",
+                            cursor: "pointer",
+                            height: "100%",
+                            backgroundColor: "rgba(255,255,255,0.4)",
+                          }}
+                        />
+                      </Fade>
+                    </Link>
+                  </div>
+                  <div>
+                    <Typography>{item.title}</Typography>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div>Nothing to show</div>
+            )}
+          </Grid>
+        )}
       </div>
 
       <Footer />
